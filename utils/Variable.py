a9ba418bfb916ba30a1affc1c1e1ca3dea510c16
Daehyun Cho <1phantasmas@korea.ac.kr>
2021-01-02 18:29:01 +0900

    [step.8] iterative backward on Var

--- utils/Variable.py
+++ utils/Variable.py
@@ -8,13 +8,24 @@ class Variable:
     def set_creator(self, func):
         self.creator = func
 
-    def backward(self):
+    def backward_recursion(self):
         f = self.creator
         if f is not None: # if f is None, then the Variable is the last node (=end of recursion)
             x = f.input # call the input before
             x.grad = f.backward(self.grad) # calculate the gradient of the function that made this Variable
             x.backward() # recursion
 
+    def backward(self): # iterative
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grda = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
+
+                
 if __name__=="__main__":
 
     import numpy as np
